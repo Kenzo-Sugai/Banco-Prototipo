fff9ab080135e3a5cda6fd95d972a9abb7f936e7
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-02-02 14:05:55 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -64,25 +64,53 @@ void menu(std::string username){
         std::cout << "Saldo: " << user->getBalance() << std::endl;
         std::cout << "Escolha uma opcao abaixo: " << std::endl;
         std::cout << "1 - Depositar" << std::endl;
-        std::cout << "2 - Extrair" << std::endl;
+        std::cout << "2 - Pagar" << std::endl;
         std::cout << "3 - Historico" << std::endl;
 
-        int n;
+        int n, choice;
 
         std::cin >> n;
 
+        std::string money;
+
         switch(n){
-
             case 1:
-
-                std::string money;
 
                 std::cout << "Quanto deseja depositar: " << std::endl;
                 std::cin >> money;
 
-                user->setBalance(money);
+                user->setBalance(money, '+');
 
                 break;
+            case 2:
+
+                std::cout << "Escolha o destino do pagamento: " << std::endl;
+                std::cout << "1 - Transferencia" << std::endl;
+                std::cout << "2 - Pagamento de Fatura" << std::endl;
+                std::cin >> choice;
+
+                if(choice == 1){
+                    std::cout << "Type the username account: " << std::endl;
+                    while(std::cin >> username){
+
+                        if(!check_valid_user(username)) break;
+
+                        std::cout << "This username not exist" << std::endl;
+                        std::cout << "Try again: " << std::endl;
+
+                    }
+
+                    std::cout << "Quanto deseja tranferir: " << std::endl;
+                    std::cin >> money;
+
+                    user->transfer(username, money);
+
+                }
+
+                break;
+            default:
+                break;
+
 
         }
 
--- private/model/Usuario.hpp
+++ private/model/Usuario.hpp
@@ -6,135 +6,194 @@ class Usuario {
 
     public:
 
-        Usuario(std::string username){
-
-            this->username = username;
-
-            std::fstream file;
-
-            std::string userPath = "./accounts/" + username + ".txt"; 
-
-            file.open(userPath, std::ios::in);
-
-            if(file.is_open()){
-
-                std::string line;
-
-                int i = 1;
-
-                while(getline(file, line)){
-
-                    switch(i){
-                        case 1:
-                            this->password = line;
-                            break;
-                        case 2:
-
-                            this->real = "";
-                            this->centavos = "";
-
-                            int j = 0;
-
-                            for(; line[j] != '.'; j++){
-
-                                this->real += line[j];
-
-                            }
-
-                            j++;
-
-                            for(; j < line.size(); j++){
-
-                                this->centavos += line[j];
-
-                            }
-
-                            break;
-                    }
-
-                    i++;
-
-                }
-
-            }
-
-            std::cout << "REAL: " << this->real << std::endl;
-            std::cout << "CENTS: " << this->centavos << std::endl;
-
-        }
-
-        void setUsername(std::string username){
-
-            this->username = username;
-
-        }
-
-        void setPassword(std::string password){
-
-            this->password = password;
-
-        }
-
-        void setBalance(std::string money){
-
-            std::string r = "", c = "";
-
-            int j = 0;
-
-            for(; money[j] != '.' && money[j] != ','; j++){
-
-                r += money[j];
-
-            }
-
-            j++;
-
-            for(; j < money.size(); j++){
-
-                c += money[j];
-
-            }
-
-            int sobra = (stoi(c) + stoi(this->centavos)) / 100;
-
-            this->real = std::to_string(stoi(this->real) + sobra + stoi(r));
-
-            this->centavos = std::to_string((stoi(c) + stoi(this->centavos)) % 100);
-
-            if(this->centavos.size() == 1) this->centavos = '0' + this->centavos; 
-
-            this->balance = this->real;
-            this->balance += '.';
-            this->balance += this->centavos;
-
-            std::cout << this->balance << std::endl;
-
-        }
-
-        std::string getUsername(){
-
-            return this->username;
-
-        }
-
-        std::string getPassword(){
-
-            return this->password;
-
-        }
-
-        std::string getBalance(){
-
-            return this->balance;
-
-        }
+        Usuario(std::string username);
+        void setUsername(std::string username);
+        void setPassword(std::string password);
+        void setBalance(std::string money, char operation);
+        std::string getUsername();
+        std::string getPassword();
+        std::string getBalance();
+        void transfer(std::string username, std::string money);
+        void parserMoney(std::string money);
     
     private:
 
         std::string username;
         std::string password;
         std::string balance;
-        std::string real;
-        std::string centavos;
+        std::string real = "00";
+        std::string centavos = "00";
+
+        void saveFile();
 
 };
+
+Usuario::Usuario(std::string username){
+
+    this->username = username;
+
+    std::fstream file;
+
+    std::string userPath = "./accounts/" + username + ".txt"; 
+
+    file.open(userPath, std::ios::in);
+
+    if(file.is_open()){
+
+        std::string line;
+
+        int i = 1;
+
+        while(getline(file, line)){
+
+            switch(i){
+                case 1:
+
+                    this->password = line;
+
+                    break;
+
+                case 2:
+
+                    parserMoney(line);
+
+                    this->balance = this->real + '.' + this->centavos;
+
+                    break;
+            }
+
+            i++;
+
+        }
+
+    }
+
+}
+
+void Usuario::parserMoney(std::string money){
+
+    int j = 0;
+    std::string aux = "";
+
+    for(; money[j] != '.' && money[j] != ','; j++){
+
+        aux += money[j];
+
+    }
+
+    this->real = std::to_string(stoi(this->real) + stoi(aux));
+
+    aux = "";
+
+    j++;
+
+    for(; j < money.size(); j++){
+
+        aux += money[j];
+
+    }
+
+    this->centavos = std::to_string(stoi(this->centavos) + stoi(aux));
+
+}
+
+void Usuario::setUsername(std::string username){
+
+    this->username = username;
+
+}
+
+void Usuario::setPassword(std::string password){
+
+    this->password = password;
+
+}
+
+void Usuario::setBalance(std::string money, char operation){
+    // TODO_soma e sub
+    if(operation == '+'){
+
+        parserMoney(money);
+
+        int sobra = stoi(this->centavos) / 100;
+
+        this->real = std::to_string(stoi(this->real) + sobra);
+
+        this->centavos = std::to_string(stoi(this->centavos) % 100);
+
+        if(this->centavos.size() == 1) this->centavos = '0' + this->centavos; 
+
+    }
+
+    if(operation == '-'){
+
+        int sobra = stoi(this->centavos) - stoi(this->real);
+        if(sobra < 0){
+
+            this->real = std::to_string(stoi(this->real) - 1);
+            this->centavos = std::to_string(100 - sobra);
+
+        }
+        else{
+
+            this->centavos = std::to_string(sobra);
+            this->real = std::to_string();
+
+        }
+
+    }
+
+    this->balance = this->real + '.' + this->centavos;
+
+    saveFile();
+
+}
+
+std::string Usuario::getUsername(){
+
+    return this->username;
+
+}
+
+std::string Usuario::getPassword(){
+
+    return this->password;
+
+}
+
+std::string Usuario::getBalance(){
+
+    return this->balance;
+
+}
+
+void Usuario::transfer(std::string username, std::string money){
+    
+    Usuario *user = new Usuario(username);
+
+    user->setBalance(money, '+');
+
+    delete user;
+
+    setBalance(money, '-');
+
+}
+
+void Usuario::saveFile(){
+
+    std::fstream file;
+
+    std::string userPath = "./accounts/" + username + ".txt"; 
+
+    file.open(userPath, std::ios::out);
+
+    if(file.is_open()){
+
+        file << this->password + '\n';
+        file << this->balance + '\n';
+
+    }
+
+    file.close();
+
+}
